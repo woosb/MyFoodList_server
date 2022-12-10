b739d1b558ff1ea096af138723d64c38fab95112
wsb <woosb0115@nate.com>
2022-12-10 17:50:37 +0900

    MyFoodList 서버 초기 세팅

---
+++ src/main/java/com/example/myfoodlist_server/MyFoodListServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfoodlist_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFoodListServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFoodListServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myfoodlist_server/MyFoodListServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfoodlist_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFoodListServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
